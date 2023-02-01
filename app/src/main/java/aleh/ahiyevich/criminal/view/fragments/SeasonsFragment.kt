11ee36d866ba5bd73198092ffa36ee64af72e319
Aleh <aleh2806@yandex.ru>
2023-02-01 17:30:11 +0300

    Добавил фрагмент с сезонами 2

--- app/src/main/java/aleh/ahiyevich/criminal/view/fragments/SeasonsFragment.kt
+++ app/src/main/java/aleh/ahiyevich/criminal/view/fragments/SeasonsFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import aleh.ahiyevich.criminal.R
 
-class SeassonsFragment : Fragment() {
+class SeasonsFragment : Fragment() {
 
 
     override fun onCreateView(

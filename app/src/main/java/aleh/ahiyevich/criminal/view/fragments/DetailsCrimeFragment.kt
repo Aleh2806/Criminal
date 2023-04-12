89e2c1a435a83765b9d77cb0dedc07c2899e6807
Aleh <aleh2806@yandex.ru>
2023-04-12 15:51:06 +0300

    Сделал фуллскрин без границ, теперь видео растягивется полностью на весь экран

--- app/src/main/java/aleh/ahiyevich/criminal/view/fragments/DetailsCrimeFragment.kt
+++ app/src/main/java/aleh/ahiyevich/criminal/view/fragments/DetailsCrimeFragment.kt
@@ -12,7 +12,9 @@ import android.os.Build
 import android.os.Bundle
 import android.view.*
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -20,12 +22,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.PlayerView
 import kotlinx.android.synthetic.main.custom_controller.*
 
 
 class DetailsCrimeFragment : Fragment(), OnItemClick {
 
     private val listDetails: ArrayList<String> = ArrayList()
+    private lateinit var playerView: PlayerView
+    private var simpleExoPlayer: ExoPlayer? = null
+
+
 
     private var _binding: FragmentDetailsCrimeBinding? = null
     private val binding: FragmentDetailsCrimeBinding
@@ -52,36 +60,33 @@ class DetailsCrimeFragment : Fragment(), OnItemClick {
 
     }
 
+
     private fun backWithFragment() {
         val numberSeason = arguments?.getString("KEY_SEASON").toString()
         binding.apply {
             detailsBack.setOnClickListener {
-                replaceFragment(
-                    SeasonsFragment.newInstance(
-                        numberSeason
-                    )
-                )
+                replaceFragment(SeasonsFragment.newInstance(numberSeason))
             }
         }
     }
 
-
+    // ExoPlayer - Подготовка к запуску и запуск по нажатию на кнопку Play
     private fun preparePlayer() {
-        val playerView = binding.playerView
+        playerView = binding.playerView
         val btnForward = playerView.findViewById<ImageView>(R.id.exo_forward)
         val btnFullScreen = playerView.findViewById<ImageView>(R.id.btn_fullscreen)
         val progressBar = binding.progressBar
 
-        val simpleExoPlayer = ExoPlayer.Builder(requireContext()).build()
+        simpleExoPlayer = ExoPlayer.Builder(requireContext()).build()
 
         btnForward.setOnClickListener {
-            val currentPosition = simpleExoPlayer.currentPosition
-            simpleExoPlayer.seekTo(currentPosition + 5000)
+            val currentPosition = simpleExoPlayer?.currentPosition
+            simpleExoPlayer?.seekTo(currentPosition!! + 5000)
         }
 
         playerView.player = simpleExoPlayer
         playerView.keepScreenOn = true
-        simpleExoPlayer.addListener(object : Player.Listener {
+        simpleExoPlayer?.addListener(object : Player.Listener {
             override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
                 super.onPlayerStateChanged(playWhenReady, playbackState)
                 if (playbackState == Player.STATE_BUFFERING) {
@@ -96,18 +101,17 @@ class DetailsCrimeFragment : Fragment(), OnItemClick {
         val videoSource = Uri.parse("https://www.rmp-streaming.com/media/big-buck-bunny-360p.mp4")
 //        val videoSource = Uri.parse("https://cdn.bitmovin.com/content/assets/art-of-motion-dash-hls-progressive/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd")
         val mediaItem = MediaItem.fromUri(videoSource)
-        simpleExoPlayer.setMediaItem(mediaItem)
-        simpleExoPlayer.prepare()
-        simpleExoPlayer.play()
+        simpleExoPlayer?.setMediaItem(mediaItem)
+        simpleExoPlayer?.prepare()
 
-        // TODO: Сделать кнопку которая переключает на полнорежимный экран видео
-        // TODO: Сам механизм работает вроде
+
         var isFullScreen = true
 
         btnFullScreen.setOnClickListener {
             if (isFullScreen) {
+                playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
                 requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                val params = playerView.layoutParams as RelativeLayout.LayoutParams
+                val params = playerView.layoutParams as ConstraintLayout.LayoutParams
                 params.width = ViewGroup.LayoutParams.MATCH_PARENT
                 params.height = ViewGroup.LayoutParams.MATCH_PARENT
                 playerView.layoutParams = params
@@ -121,9 +125,9 @@ class DetailsCrimeFragment : Fragment(), OnItemClick {
                 isFullScreen = false
             } else {
               requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-                val params = playerView.layoutParams as RelativeLayout.LayoutParams
+                val params = playerView.layoutParams as ConstraintLayout.LayoutParams
                 params.width = ViewGroup.LayoutParams.MATCH_PARENT
-                params.height = 270 * (requireActivity().resources.displayMetrics.density).toInt()
+                params.height = 230 * (requireActivity().resources.displayMetrics.density).toInt()
                 playerView.layoutParams = params
                 binding.materialsTittle.visibility = View.VISIBLE
                 btnFullScreen.setImageDrawable(
@@ -135,24 +139,31 @@ class DetailsCrimeFragment : Fragment(), OnItemClick {
                 isFullScreen = true
             }
         }
+    }
 
+    private fun releasePlayer(){
+        simpleExoPlayer.let { player->
+            player?.release()
+            simpleExoPlayer = null
+        }
     }
 
 
     override fun onStop() {
         super.onStop()
-        // TODO: Добавить метод release и обнулить плейер
+        releasePlayer()
     }
 
     override fun onPause() {
         super.onPause()
-        // TODO: Добавить метод release и обнулить плейер
+        releasePlayer()
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        // TODO: Добавить метод release и обнулить плейер
+        releasePlayer()
     }
 
 
